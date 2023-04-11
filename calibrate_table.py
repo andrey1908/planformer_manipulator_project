3aa24886197649dde1e19aa6dbaef0b990a9ff17
andrey1908 <andrey.krishtopik@mail.ru>
2023-04-11 14:22:13 +0300

    calibrate_table.py: calibrate_table() function processes only side images, aruco_dict and params are passed in arguments, 'corners_3d' are returned in original shape (n, 4, 3) instead of (n * 4, 3)

--- calibrate_table.py
+++ calibrate_table.py
@@ -5,19 +5,13 @@ from utils.aruco import detect_aruco, select_aruco_poses, select_aruco_markers, 
 from utils.estimate_plane_frame import estimate_plane_frame
 
 
-def calibrate_table(image, aruco_size, K, D, caemra_position):
-    assert caemra_position in ("side", "top")
-    aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_1000)
-    params = cv2.aruco.DetectorParameters_create()
+def calibrate_table(image, aruco_size, K, D, aruco_dict, params):
     arucos = detect_aruco(image, K=K, D=D, aruco_sizes=aruco_size,
         use_generic=True, aruco_dict=aruco_dict, params=params)
-    if caemra_position == "side":
-        arucos = select_aruco_poses(arucos, PoseSelectors.Z_axis_up)
-    elif caemra_position == "top":
-        arucos = select_aruco_poses(arucos, PoseSelectors.Z_axis_back)
+    arucos = select_aruco_poses(arucos, PoseSelectors.Z_axis_up)
     arucos = select_aruco_markers(arucos, lambda id: id < 4)
-    if len(arucos.n) != 4:
+    if arucos.n != 4:
         return None, None
-    corners_3d = get_aruco_corners_3d(arucos).reshape(16, 3)
-    camera2table = estimate_plane_frame(corners_3d)
+    corners_3d = get_aruco_corners_3d(arucos)
+    camera2table = estimate_plane_frame(corners_3d.reshape(16, 3))
     return camera2table, corners_3d
