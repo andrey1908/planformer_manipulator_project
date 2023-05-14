92f9da8d97e88fb30970f3042db0860ffdecf1a2
andrey1908 <andrey.krishtopik@mail.ru>
2023-05-14 22:41:48 +0300

    segmentation.py: adjust thresholds; segment_scene.ipynb: minor changes

--- segmentation.py
+++ segmentation.py
@@ -7,64 +7,64 @@ from segment_by_color import segment_by_color
 def segment_red_boxes_hsv(hsv, view=""):
     assert view in ("top", "front", "")
     # shift hue so that red color is continuous
-    hsv = hsv + np.array([150, 0, 0], dtype=np.uint8).reshape(1, 1, 3)
-    min_color = np.array([147 - 9, 110, 120], dtype=np.uint8)
-    max_color = np.array([147 + 9, 255, 255], dtype=np.uint8)
+    hsv = hsv + np.array([100, 0, 0], dtype=np.uint8)
+    min_color = np.array([63, 110, 90], dtype=np.uint8)
+    max_color = np.array([106, 255, 255], dtype=np.uint8)
     if view:
         x_range, y_range = segmentation_roi[view]["working_area"]
     else:
         x_range, y_range = slice(0, None), slice(0, None)
     mask, polygons = segment_by_color(hsv, min_color, max_color,
         x_range=x_range, y_range=y_range,
-        refine=True, min_polygon_length=100, max_polygon_length=1000)
+        refine=True, min_polygon_length=80, max_polygon_length=250)
     return mask, polygons
 
 
 def segment_blue_boxes_hsv(hsv, view=""):
     assert view in ("top", "front", "")
-    min_color = np.array([161 - 9, 110, 120], dtype=np.uint8)
-    max_color = np.array([161 + 9, 255, 255], dtype=np.uint8)
+    min_color = np.array([132, 110, 90], dtype=np.uint8)
+    max_color = np.array([186, 255, 255], dtype=np.uint8)
     if view:
         x_range, y_range = segmentation_roi[view]["working_area"]
     else:
         x_range, y_range = slice(0, None), slice(0, None)
     mask, polygons = segment_by_color(hsv, min_color, max_color,
         x_range=x_range, y_range=y_range,
-        refine=True, min_polygon_length=100, max_polygon_length=1000)
+        refine=True, min_polygon_length=80, max_polygon_length=250)
     return mask, polygons
 
 
 def segment_goal_hsv(hsv, view=""):
     assert view in ("top", "front", "")
-    min_color = np.array([45 - 9, 60, 160], dtype=np.uint8)
-    max_color = np.array([45 + 9, 255, 255], dtype=np.uint8)
+    min_color = np.array([36, 50, 150], dtype=np.uint8)
+    max_color = np.array([68, 255, 255], dtype=np.uint8)
     if view:
         x_range, y_range = segmentation_roi[view]["goal_and_stop_line"]
     else:
         x_range, y_range = slice(0, None), slice(0, None)
     mask, polygons = segment_by_color(hsv, min_color, max_color,
         x_range=x_range, y_range=y_range,
-        refine=True, min_polygon_length=100, max_polygon_length=3000)
+        refine=True, min_polygon_length=150, max_polygon_length=1500)
     return mask, polygons
 
 
 def segment_stop_line_hsv(hsv, view=""):
     assert view in ("top", "front", "")
     min_color = np.array([0, 0, 0], dtype=np.uint8)
-    max_color = np.array([255, 255, 80], dtype=np.uint8)
+    max_color = np.array([255, 255, 100], dtype=np.uint8)
     if view:
         x_range, y_range = segmentation_roi[view]["goal_and_stop_line"]
     else:
         x_range, y_range = slice(0, None), slice(0, None)
     mask, polygons = segment_by_color(hsv, min_color, max_color,
         x_range=x_range, y_range=y_range,
-        refine=True, min_polygon_length=100, max_polygon_length=1000)
+        refine=True, min_polygon_length=500, max_polygon_length=900)
     return mask, polygons
 
 
 def segment_table_markers_hsv(hsv, view=""):
     assert view in ("top", "front", "")
-    min_color = np.array([90, 90, 100], dtype=np.uint8)
+    min_color = np.array([90, 60, 80], dtype=np.uint8)
     max_color = np.array([130, 255, 255], dtype=np.uint8)
     if view:
         x_range, y_range = segmentation_roi[view]["working_area"]
@@ -72,6 +72,6 @@ def segment_table_markers_hsv(hsv, view=""):
         x_range, y_range = slice(0, None), slice(0, None)
     (refined_mask, orig_mask), polygons = segment_by_color(hsv, min_color, max_color,
         x_range=x_range, y_range=y_range,
-        refine=True, min_polygon_length=30, max_polygon_length=100,
+        refine=True, min_polygon_length=30, max_polygon_length=150,
         return_orig_mask=True)
     return (refined_mask, orig_mask), polygons
