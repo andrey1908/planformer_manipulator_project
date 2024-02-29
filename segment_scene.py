9e84e2a03b047165a43da89229bb5c76e1940a5d
andrey1908 <andrey.krishtopik@mail.ru>
2024-02-29 17:37:02 +0300

    segment_scene.py: segment_scene_colorful() function draws table markers

--- segment_scene.py
+++ segment_scene.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 from segmentation import segment_red_boxes_hsv, segment_blue_boxes_hsv, \
-    segment_goal_hsv, segment_stop_line_hsv, segmnet_nn
+    segment_goal_hsv, segment_stop_line_hsv, segmnet_nn, segment_table_markers_hsv
 from params import use_nn
 
 
@@ -14,6 +14,7 @@ def segment_scene_colorful(image, view=""):
         blue_mask, blue_polygons = segment_blue_boxes_hsv(hsv, view=view)
         goal_mask, goal_polygons = segment_goal_hsv(hsv, view=view)
         stop_line_mask, stop_line_polygons = segment_stop_line_hsv(hsv, view=view)
+        (tm_refined_mask, tm_mask), tm_polygons = segment_table_markers_hsv(hsv, view=view)
     else:
         red_mask, blue_mask, goal_mask, stop_line_mask, \
             red_polygons, blue_polygons, goal_polygons, stop_line_polygons = \
@@ -21,8 +22,8 @@ def segment_scene_colorful(image, view=""):
 
     num_red = len(red_polygons)
     num_blue = len(blue_polygons)
-    num_goals = len(goal_polygons)
-    num_stop_lines = len(stop_line_polygons)
+    # num_goals = len(goal_polygons)
+    # num_stop_lines = len(stop_line_polygons)
 
     # assert num_goals == 3
     # assert num_stop_lines == 1
@@ -33,7 +34,8 @@ def segment_scene_colorful(image, view=""):
     segmentation[:, :, 1] = np.maximum(segmentation[:, :, 1], goal_mask)
     segmentation[:, :, 2] = np.maximum(segmentation[:, :, 2], goal_mask)
     segmentation[:, :, 1] = np.maximum(segmentation[:, :, 1], stop_line_mask)
-    segmentation[segmentation == 0] = 60
+    segmentation[:, :, 1] = np.maximum(segmentation[:, :, 1], tm_refined_mask)
+    segmentation[segmentation == 0] = 0
     return segmentation, (num_red, num_blue)
 
 
